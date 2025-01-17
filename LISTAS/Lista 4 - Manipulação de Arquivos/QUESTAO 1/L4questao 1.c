b622525ff8087f050e3f5d8fdad93850fdaf9f25
Kauã Pires Ribeiro <pireskauan998@gmail.com>
2025-01-17 10:32:23 -0300

    Lista 4.c

--- LISTAS/Lista 4 - Manipulação de Arquivos/QUESTAO 1/L4questao 1.c
+++ LISTAS/Lista 4 - Manipulação de Arquivos/QUESTAO 1/L4questao 1.c
@@ -5,7 +5,10 @@ int main(){
   if(arquivo == NULL){
     printf("Erro ao abrir o arquivo");
   }
- fprintf(arquivo, "Red Dead Redemption 2 e uma obra-prima dos jogos de mundo aberto, oferecendo uma experiencia imersiva e emocional. Com graficos deslumbrantes, narrativa profunda e personagens cativantes, ele transporta os jogadores para o Velho Oeste. Cada detalhe, desde a ambientacao ate as interacoes, contribui para uma jornada inesquecivel.");
+    fprintf(arquivo,"Red Dead Redemption 2 e uma obra-prima dos jogos de mundo aberto, oferecendo uma experiencia imersiva e emocional.\n");
+    fprintf(arquivo,"Com graficos deslumbrantes, narrativa profunda e personagens cativantes, ele transporta os jogadores para o Velho Oeste.");
+    fprintf(arquivo,"Cada detalhe, desde a ambientacao ate as interacoes, contribui para uma jornada inesquecivel.\n");
+
   fclose(arquivo);
     return 0;
 }
--- LISTAS/Lista 4 - Manipulação de Arquivos/QUESTAO 3/L4questao 3.c
+++ LISTAS/Lista 4 - Manipulação de Arquivos/QUESTAO 3/L4questao 3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     FILE *arquivo = fopen("saida.txt", "a");
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo");
     }
-    fprintf(arquivo, "A liberdade para explorar, cacar, pescar e interagir com o ambiente cria um mundo vivo e realista. Alem disso, sua trilha sonora e mecanicas de jogo elevam ainda mais a experiencia, tornando-o um marco na historia dos videogames.");
+    fprintf(arquivo,"A liberdade para explorar, cacar, pescar e interagir com o ambiente cria um mundo vivo e realista.\n");
+    fprintf(arquivo,"Alem disso, sua trilha sonora e mecanicas de jogo elevam ainda mais a experiencia, tornando-o um marco na historia dos videogames.\n");
     fclose(arquivo);
     return 0;
 }
